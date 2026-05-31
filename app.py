7568b7bd3670b7de776093689afd857f5ecd73d2
Max <81037708+maxgerbens@users.noreply.github.com>
2026-05-31 22:01:52 +0200

    Remove app from sidebar, use explicit navigation

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
-st.switch_page("pages/1_Trendweergave.py")
+pages = [
+    st.Page("pages/1_Trendweergave.py", title="Trendweergave"),
+    st.Page("pages/2_Vergelijkingsweergave.py", title="Vergelijkingsweergave"),
+]
+
+pg = st.navigation(pages)
+pg.run()
